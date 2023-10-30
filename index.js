162732ef8ecd2c522a563551f1378918ea3c5fab
Artemijs Matusev <matu6ev@gmail.com>
2023-10-30 23:13:55 +0000

    Set up a HTML form and /upload endpoint on the application to handle/save image uploads on the server

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 //Imports
 import express from "express";
+import fileUpload from "express-fileupload";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 
 
 
@@ -7,8 +12,10 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+
 //Set up folder for the static files
 app.use(express.static("public"));
+app.use(fileUpload());
 
 
 //setting up the server
@@ -16,6 +23,15 @@ app.listen(port, () => {
     console.log(`Server running on port: ${port}`);
 })
 
+
+//Handling the post /upload route
+app.post("/upload", (req, res) => {
+    console.log(req.files);
+
+    const image = req.files.image;
+
+    image.mv(__dirname + '/tmp/' + image.name);
+})
 
 
 //GET the root of the website
